e2bec9751429a51c1fbb79833a82809503f5b493
ぱる！ <sunset005goo@gmail.com>
2022-03-05 03:32:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,65 @@
-const { Client, Intents, Interaction, Message } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], restTimeOffset: 0 });
+//定義系
+const http = require("http");
+const {Client,Intents,MessageEmbed} = require("discord.js");
+const client = new Client({ intents: Object.keys(Intents.FLAGS)});
+const {Signale} = require("signale");
+const logger = new Signale();
+const Keyv = require('keyv')
+const token = process.env.BOT_TOKEN;
+const prefix = process.env.BOT_PREFIX;
 
-const prefix = "!";
-
+//起動構成
 client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}.`);
-});
+	logger.success(`${client.user.tag}でログイン済み`);
+	setInterval(() => {
+  		var i = 4
+  		if (i % 4 === 0) {
+    		client.user.setActivity(`${prefix}help | ${client.guilds.cache.size}サーバー`, {type: "WATCHING" });
+		};
+		var i = 7
+		if (i % 4 === 3) {
+    		client.user.setActivity(`${prefix}ping | ${client.guilds.cache.map(guild => guild.memberCount).reduce((p, c) => p + c)}人のユーザー`, {type: "WATCHING" });
+		};
+	}, 5000);
+})
 
-/**
- *
- * @param {Interaction} interaction
- */
-client.on("interactionCreate", async (interaction) => {
-  if (interaction.isCommand()) {
-    const { commandName:command, options } = interaction;
-    if (command === "eval") {
-      if (!["eval実行可能なユーザーID"].includes(interaction.user.id)) return interaction.reply({ content: "あなたはこのコマンドを実行できません。", ephemeral: true });
-      await interaction.deferReply();
-      const code = options.getString("code");
-      const result = new Promise((resolve) => resolve(eval(code)));
-      return result
-        .then(async (output) => {
-          if (typeof output !== "string") {
-            output = require("util").inspect(output, { depth: 0 });
-          }
-          if (output.includes(client.token)) {
-            output = output.replace(client.token, "[TOKEN]");
-          }
-          interaction.followUp(`\`\`\`js\n${output}\n\`\`\``);
-        })
-        .catch(async (err) => {
-          err = err.toString();
-          if (err.includes(client.token)) {
-            err = err.replace(client.token, "[TOKEN]");
-          }
-          interaction.followUp(`\`\`\`js\n${err}\n\`\`\``);
-        });
-    }
-  }
-});
-
-/**
- *
- * @param {Message} message
- */
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
+	if (!message.author.id === "716343156513439845") return; //公開時に消す
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  if (command === "eval") {
-    if (!["eval実行可能なユーザーID"].includes(message.author.id)) return message.reply({ content: "あなたはこのコマンドを実行できません。" });
-    const code = args.join(" ");
-    const result = new Promise((resolve) => resolve(eval(code)));
-    return result
-      .then(async (output) => {
-        if (typeof output !== "string") {
-          output = require("util").inspect(output, { depth: 0 });
-        }
-        if (output.includes(client.token)) {
-          output = output.replace(client.token, "[TOKEN]");
-        }
-        message.reply(`\`\`\`js\n${output}\n\`\`\``);
-      })
-      .catch(async (err) => {
-        err = err.toString();
-        if (err.includes(client.token)) {
-          err = err.replace(client.token, "[TOKEN]");
-        }
-        message.reply(`\`\`\`js\n${err}\n\`\`\``);
-      });
-  }
+	if(command === "help") {
+		message.reply({
+			embeds: [new MessageEmbed()
+				.setTitle(":question:ヘルプ")
+				.setDescription("すべてのコマンドは`" + prefix + "`で始まる必要があります。")
+			]
+		, allowedMentions : {repliedUser : false}})
+	} else if (command === "emit") {
+		message.reply({
+			embeds: [new MessageEmbed()
+				.setTitle(":warning:エラー")
+				.setDescription("開発環境用テストコマンドは現在設定されていないため使用できません。")
+				.setColor("#ff0000")
+				.setFooter({text: "ErrorCode:00-2(Emit command not available)"})
+			]
+		})
+	} else {
+		message.reply({
+			embeds: [new MessageEmbed()
+				.setTitle(":warning:エラー")
+				.setDescription("`" + message.content + "`というコマンドが見つかりませんでした！")
+				.setFooter({text: `ErrorCode:00-1(Command not found)`})
+				.setColor("RED")
+			]
+		, allowedMentions : {repliedUser : false}})
+	}
 });
 
-client.login(process.env.BOT_TOKEN);
+http.createServer(function(req, res){
+   res.write("OK");
+   res.end();
+}).listen(8080);
+
+client.login(token);
